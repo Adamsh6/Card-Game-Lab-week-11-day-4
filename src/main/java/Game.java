f30d0238b60c600e08aded19202128c36df9fae7
StewartMacLeman <stewart_macleman@hotmail.com>
2019-07-18 14:08:58 +0100

    Game class added

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+
 public class Game {
+
+    private Deck deck;
+    private ArrayList<Player> players;
+
+    public Game(ArrayList<Player> players) {
+        this.players = players;
+        this.deck = new Deck();
+        this.deck.setUpCards();
+        this.deck.shuffle();
+    }
+
+    public void dealCards() {
+        for(Player player: this.players){
+            Card card = this.deck.dealCard();
+            player.takeCard(card);
+        }
+    }
+
+    public Player checkRoundWinner() {
+        Player currentHighest = this.players.get(0);
+        for(int i = 1 ; i < this.players.size(); i++){
+            Player player = this.players.get(i);
+            if (currentHighest.getCard().getValueFromEnum() < player.getCard().getValueFromEnum()){
+                currentHighest = player;
+            }
+        }
+        currentHighest.addPoint();
+        return currentHighest;
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,52 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class GameTest {
+
+    Player player1;
+    Player player2;
+    Game game;
+
+    @Before
+    public void setUp() {
+        player1 = new Player("John");
+        player2 = new Player("Jim");
+        ArrayList<Player> players = new ArrayList<Player>();
+        players.add(player1);
+        players.add(player2);
+        game = new Game(players);
+    }
+
+    @Test
+    public void canCardsBeDealt() {
+        game.dealCards();
+        assertEquals(1, player1.countCards());
+        assertEquals(1, player2.countCards());
+    }
+
+    @Test
+    public void checkRoundWinnerP1Win() {
+        Card card1 = new Card(SuitType.HEARTS, RankType.QUEEN);
+        player1.takeCard(card1);
+        Card card2 = new Card(SuitType.HEARTS, RankType.THREE);
+        player2.takeCard(card2);
+        assertEquals(player1, game.checkRoundWinner());
+        assertEquals(1, player1.getPoints());
+        assertEquals(0, player2.getPoints());
+    }
+
+    @Test
+    public void checkRoundWinnerP2Win() {
+        Card card1 = new Card(SuitType.HEARTS, RankType.THREE);
+        player1.takeCard(card1);
+        Card card2 = new Card(SuitType.HEARTS, RankType.QUEEN);
+        player2.takeCard(card2);
+        assertEquals(player2, game.checkRoundWinner());
+        assertEquals(0, player1.getPoints());
+        assertEquals(1, player2.getPoints());
+    }
 }
